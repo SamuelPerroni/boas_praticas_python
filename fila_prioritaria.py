63912a0baa1d5dec69f4690055266048465ab9c8
Samuel Perroni <samuel_perroni@outlook.com>
2024-03-22 16:39:26 -0300

    Adicionando constantes e metodo template

--- fila_prioritaria.py
+++ fila_prioritaria.py
@@ -1,21 +1,17 @@
 from fila_base import FilaBase
+from constantes import CODIGO_PRIORITARIO
 
 
 class FilaPrioritaria(FilaBase):
     def gera_senha_atual(self) -> None:
-        self.senha_atual = f'PR{self.codigo}'
-
-    def atualiza_fila(self) -> None:
-        self.reseta_fila()
-        self.gera_senha_atual()
-        self.fila.append(self.senha_atual)
+        self.senha_atual = f'{CODIGO_PRIORITARIO}{self.codigo}'
 
     def chama_cliente(self, caixa: int) -> str:
         cliente_atual = self.fila.pop(0)
         self.clientes_atendidos.append(cliente_atual)
         return f'Cliente atual: {cliente_atual}, dirija-se ao caixa: {caixa}'
 
-    def estatistica(self, dia: str, agencia: int, flag: str = '') -> dict:
+    def estatistica(self, dia: int, agencia: int, flag: str = '') -> dict:
         if flag != 'detail':
             estatistica = {f'{agencia}-{dia}': len(self.clientes_atendidos)}
         else:
